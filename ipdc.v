fc9ea5e12c12fcf5f107b965d40471eef193e43a
Your Name <you@example.com>
2020-10-29 00:27:20 +0800

    keep going and still something don't understand

--- ipdc.v
+++ ipdc.v
@@ -16,16 +16,26 @@ module ipdc (                       //Don't modify interface
 // ---------------------------------------------------------------------------
 // ---- Add your own wires and registers here if needed ---- //
 
-reg 			 o_in_ready_w, o_in_ready_r;
+//output register
+reg 	   o_in_ready_w, o_in_ready_r;
 reg        o_out_valid_w, o_out_valid_r;
 reg [23:0] o_out_data_w, o_out_data_r;
 
+//input register
 reg        i_op_valid_w;
 reg [ 2:0] i_op_mode_w;
-reg 			 i_in_valid_w;
+reg 	   i_in_valid_w;
 reg [23:0] i_in_data_w;      
 
-reg [ 2:0] fsm_state;
+//fsm state
+reg [ 2:0]  fsm_state;
+
+//store input image
+reg [23:0]  input_img [0:63];
+
+//store output image
+reg [23:0] output_img [0:15];
+
 // ---------------------------------------------------------------------------
 // Continuous Assignment
 // ---------------------------------------------------------------------------
@@ -54,16 +64,21 @@ end
 //reset signal
 always@(posedge i_clk or negedge i_rst_n)begin   
 	if(!i_rst_n)begin   
-		
+		//initialize
 	end
-	else begin
-		
+	else begin 
+		o_in_ready_r <= o_in_ready_w;
+		o_out_valid_r <= o_out_valid_w;
+		o_out_data_r <= o_out_data_w;
 	end
 end
 
 //All inputs are synchronized with the negative edge clock
 always(@negedge i_clk)begin
-	if 
+	 i_op_valid_w <= i_op_valid;
+	 i_op_mode_w  <= i_op_mode;
+	 i_in_valid_w <= i_in_valid;
+	 i_in_data_w  <= i_in_data;
 end
 
 endmodule
